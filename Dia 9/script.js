abcf2de5a567e1ca0e8a7d5e208b5349b3e78918
BrayanStivenMaldonado <stivenmaldonado0805@gmail.com>
2024-09-03 07:48:04 -0500

    feat: :sparkles: created the view of the PokeId

--- Dia 9/script.js
+++ Dia 9/script.js
@@ -1,6 +1,10 @@
 document.getElementById("Next").addEventListener("click", MostrarNombre())
 
-
+button.addEventListener("keyup", MostrarPokemon()){
+    if (event.keyCode == 13){
+        button.click()
+    }
+}
 
 function MostrarPokemon(){
     let id = document.getElementById("Find").value;
@@ -8,17 +12,23 @@ function MostrarPokemon(){
     fetch(url)
     .then(res => res.json())
     .then(Data => {
+        //Mostar el Gif del Pokemon
         let image = document.getElementById("Image")
         image.innerHTML = `
-        <img src="${Data.sprites.other.showdown.back_default}">
+        <img src="${Data.sprites.other.showdown.front_default}">
         `
 
-
+        //Mostar el id del Pokemon
+        let Id = document.getElementById("PokeId")
+        Id.innerHTML = `
+        ${Data.id+"ﾠ-ﾠ"}
+        `
 
         //Mostar el nombre del Pokemon
         let Name = document.getElementById("PokeName")
         Name.innerHTML = `
         <p>${Data.name}</p>
         `
+
     })
 }
